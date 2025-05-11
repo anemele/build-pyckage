7dacd88ed409da23005406f692adea259a8fbdc6
Jason Swift <anemele@outlook.com>
2025-05-11 18:53:05 +0800

    add -trimpath flag to go build

--- make.bat
+++ make.bat
@@ -6,4 +6,4 @@ set output=load-pyckage
 set dist=dist
 if not exist %dist% mkdir %dist%
 
-go build -ldflags="-s -w" -o %dist%\%output%.exe
+go build -ldflags="-s -w" -trimpath -o %dist%\%output%.exe
