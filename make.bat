f9ac00b12acbcf04c12954066c8ddc36ae162442
Jason Swift <anemele@outlook.com>
2025-06-14 17:10:37 +0800

    move go code into a subdirectory

--- make.bat
+++ make.bat
@@ -1,9 +1,13 @@
 @echo off
 
+pushd %~dp0
+
 setlocal
 
 set output=load-pyckage
 set dist=dist
 if not exist %dist% mkdir %dist%
 
-go build -ldflags="-s -w" -trimpath -o %dist%\%output%.exe
+go build -ldflags="-s -w" -trimpath -o %dist%\%output%.exe ./load-pykg
+
+popd
